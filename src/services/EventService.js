f2122021e4c2854f51d2c46b30c53e185f574ee8
Evan Tolstof <itolstof@gmail.com>
2023-05-01 17:36:13 +0700

    fix message preparator

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -9,6 +9,87 @@ class EventService {
     return result;
   }
 
+  prepareTagsMarkdown(tags) {
+    if (!tags || !tags.length) {
+      return "";
+    }
+
+    return tags
+      .replaceAll(" ", "")
+      .replaceAll("_", "\\_")
+      .replaceAll("-", "\\_")
+      .split(",")
+      .map((tag) => `#${tag}`)
+      .join(" ");
+  }
+
+  prepareAddressMarkdown({ country, city, address, geo }) {
+    if (!country || !city || !address) {
+      return "WORLD (Location was not provided)";
+    }
+
+    const address = `${country}, ${city}, ${address}\n${
+      geo.length
+        ? "*MAP LINK:* " + `[Click to open](${data.event_location.geo})`
+        : ""
+    }`;
+
+    return address.replaceAll("_", "\\_");
+  }
+
+  prepareEventLinkMarkdown(link) {
+    if (!link) {
+      return null;
+    }
+
+    return `[Link to event](${link})`;
+  }
+
+  prepareOrganizerMarkdown({
+    event_organizer_credentials,
+    event_organizer_telegram_link,
+  }) {
+    if (!event_organizer_credentials || !event_organizer_telegram_link) {
+      return "Anonymous";
+    }
+
+    const result = `${event_organizer_credentials} ${event_organizer_telegram_link}`;
+
+    return result.replaceAll("_", "\\_");
+  }
+
+  prepareDescription(description) {
+    if (!description || !description.length) {
+      return "";
+    }
+
+    return description.replaceAll("_", "\\_").replaceAll("@", "\\@");
+  }
+
+  prepareEventMessageToAdminCheck(data) {
+    if (!data) {
+      return "";
+    }
+
+    const tags = this.prepareTagsMarkdown(data?.event_tags);
+    const address = this.prepareAddressMarkdown(data.event_location);
+    const linkToEvent = this.prepareEventLinkMarkdown(data.event_link);
+    const organizer = this.prepareOrganizerMarkdown(data);
+    const description = this.prepareDescription(data.event_description);
+
+    const eventAgendaMessage = `||${
+      data.event_id
+    }||\n\n${organizer} want to publish information about event:\n\n*${
+      data.event_name
+    }*\n\n${description}\n\n*DATE and TIME:* ${data.event_date}\n*TYPE:* ${
+      data.event_is_offline ? "Offline" : "Online"
+    }\n\n${data.event_is_offline ? "*ADDRESS:* " : "*LINK:* "}${
+      data.event_is_offline ? address : linkToEvent
+    }\n\n${tags}`;
+
+    return eventAgendaMessage;
+  }
+
   prepareEventMessageToPublish(data) {
     if (!data) {
       return "";
@@ -45,49 +126,6 @@ class EventService {
     }${data.event_is_offline ? address : linkToEvent}\n\n${tags}`;
 
     return eventMessage;
-  }
-
-  prepareEventMessageToAdminCheck(data) {
-    if (!data) {
-      return "";
-    }
-
-    const tags = data?.event_tags?.length
-      ? data.event_tags
-          .replaceAll(" ", "")
-          .replaceAll("_", "\\_")
-          .split(",")
-          .map((tag) => `#${tag.replaceAll("-", "\\_")}`)
-          .join(" ")
-      : "";
-
-    console.log(tags);
-
-    const address = `${data.event_location.country}, ${
-      data.event_location.city
-    }, ${data.event_location.address}\n${
-      data.event_location.geo.length
-        ? "*MAP LINK:* " + `[Click to open](${data.event_location.geo})`
-        : ""
-    }`;
-
-    const linkToEvent = `[Link to event]( ${data.event_link} )`;
-
-    const organizer = `${
-      data.event_organizer_credentials
-    } ${data.event_organizer_telegram_link.replaceAll("_", "\\_")}`;
-
-    const eventAgendaMessage = `||${
-      data.event_id
-    }||\n\n${organizer} want to publish information about event:\n\n*${
-      data.event_name
-    }*\n\n${data.event_description}\n\n*DATE and TIME:* ${
-      data.event_date
-    }\n*TYPE:* ${data.event_is_offline ? "Offline" : "Online"}\n\n${
-      data.event_is_offline ? "*ADDRESS:* " : "*LINK:* "
-    }${data.event_is_offline ? address : linkToEvent}\n\n${tags}`;
-
-    return eventAgendaMessage;
   }
 }
 
