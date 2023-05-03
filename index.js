649bf39d3fe94e0c76c8a0fea4c554d1f20f19af
Evan Tolstof <itolstof@gmail.com>
2023-05-03 17:39:46 +0700

    simplifying message

--- index.js
+++ index.js
@@ -113,11 +113,11 @@ if (process.env.NODE_ENV === "production") {
 
           const tgLink = "link";
 
-          const message = `*It's a match!!!*\n\nLooks like you have something to discuss with ${fullName}\n___________________________\n Some info about your contact:\n${description}\n\nDo not wait, text immediately: ${tgLink} !\n\n*Good luck, hope this connection bring you new opportunities!*  \❤️`;
+          const message = `*It's a match!!!*\n\nLooks like you have something to discuss with ${fullName}\n___________________________\n Some info about your contact:\n${description}\n\nDo not wait, text immediately: ${tgLink} !\n\n*Good luck, hope this connection bring you new opportunities!*`;
 
           console.log(message);
 
-          await BotHelper.send(bot, currentUser.user_id, message, {
+          await BotHelper.send(bot, currentUser.user_id, "*Test*", {
             parse_mode: "Markdown",
           });
         });
